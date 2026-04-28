c7e424e25d694b0e6a706522ddb1a7a2d18811c4
Sriniketh-Vangipuram <sriniketh202@gmail.com>
2026-04-28 23:58:33 +0530

    Modified wsgi.py

--- backend/dehazing_app/wsgi.py
+++ backend/dehazing_app/wsgi.py
@@ -11,6 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dehazing_app.settings")
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.dehazing_app.settings')
 application = get_wsgi_application()
